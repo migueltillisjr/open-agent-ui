aeada55a05f909a6cd33fc22af666c70d1539a49
migueltillisjr <migueltillisjr@infopioneer.dev>
2025-01-15 23:52:54 -0800

    prep to rebuild responsive menu

--- agent/templates/js/responsive.index.js
+++ agent/templates/js/responsive.index.js
@@ -2,8 +2,6 @@ function responsiveMenu() {
     const sidebar = document.getElementById('sidebar');
     if (sidebar) {
         sidebar.classList.toggle('active');
-        console.log(sidebar)
-        console.log('Sidebar element FOUND!');
     } else {
         console.error('Sidebar element not found!');
     }
